8f08f23ed8bb629bdbdab7a338f467c7abfecc9d
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2020-08-16 16:52:31 +0800

    Delete DateIllegalException.java

--- java/com/example/myproject/service/Exceptions/DateIllegalException.java
+++
@@ -1,8 +0,0 @@
-package com.example.myproject.service.Exceptions;
-
-public class DateIllegalException extends Exception {
-	public String message = "日期不合法";
-	public DateIllegalException() {
-		
-	}
-}
